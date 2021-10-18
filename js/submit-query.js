43175f36c2980468e54d0fb85ff9a27b0e7595d5
ansh nagrath <anshnagrath448@gmail.com>
2021-10-18 09:15:20 +0530

    changed messages

---
+++ js/submit-query.js
@@ -0,0 +1,40 @@
+$(document).ready( function() {
+    $("#contactForm").on('submit', function (e) {
+   
+       let payload = { "name" : $('#name').val() , "number" : $('#phonenumber').val() , "email": $('#email').val() , "projectName" :"Elan Empire" ,"message" : "-"   }
+       $.ajax({
+           type: 'POST',
+           url: "https://n3jhbh6bg8.execute-api.ap-south-1.amazonaws.com/default/EmailShooter",
+        
+           data: JSON.stringify(payload),
+           success:function(data){
+             console.log(JSON.stringify(data,null,2),"====>success");
+             $('#success').html("<div class='alert alert-success'>");
+                            //   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            //    .append( "</button>");
+                             $('#success > .alert-success')
+                               .append("<strong>Your message has been sent. </strong>");
+                      $('#success > .alert-success')
+                        .append('</div>');
+                                        
+                      //clear all fields
+                      $('#contactForm').trigger("reset");
+   
+            
+           },
+           error:function(data){
+               console.log("Error Occured ===>", JSON.stringify(data,null,2));
+   
+               alert("Error Occured ===>",JSON.stringify(data,null,2));
+               $('#success').html("<div class='alert alert-danger'>");
+ 
+                   $('#success > .alert-danger').append("<strong>Sorry "+  $("input#name").val()  +" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:Choudharypreeti211993@gmail.com?Subject=Message_Me from myprogrammingblog.com'>Choudharypreeti211993@gmail.com</a> ? Sorry for the inconvenience!");
+                $('#success > .alert-danger').append('</div>');
+            //clear all fields
+                $('#contactForm').trigger("reset");
+           }
+       });
+       e.preventDefault();
+   
+    })
+   })
